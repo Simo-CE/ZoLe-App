386720500bb98f0a3bc4cca62a141778c1571f22
Simo-CE <70328701+Simo-CE@users.noreply.github.com>
2021-12-02 01:25:46 +0100

    'seventh'

--- app/src/main/java/com/smiley/yo/ServicesActivity.java
+++ app/src/main/java/com/smiley/yo/ServicesActivity.java
@@ -13,52 +13,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ServicesActivity extends AppCompatActivity {
-    //private List<Service> services_list;
-
-    @Override
-    /*protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_services);
-
-        Intent intent = getIntent();
-
-        ListView services_list = (ListView) findViewById(android.R.id.list);
-
-        *//*
-        final ArrayList<Service> services_array = new ArrayList<>();
-        Resources resources = getResources();
-        final String[] title = resources.getStringArray(R.array.service_title);
-        final int[] price = resources.getIntArray(R.array.service_price);
-        for (int i = 0; i < title.length; ++i) {
-            services_array.add(new Service(title[i], price[i]));
-        }
-        *//*
-        final ArrayList<Service> services_array = new ArrayList<>();
-        services_array.add(new Service("s1", 1));
-
-        ServicesAdapter itemsAdapter = new ServicesAdapter(this, services_array);
-        services_list.setAdapter(itemsAdapter);
-
-        *//*
-        services_list = new ArrayList<>();
-
-        Resources resources = getResources();
-        final String[] title = resources.getStringArray(R.array.service_title);
-        final int[] price = resources.getIntArray(R.array.service_price);
-        for (int i = 0; i < title.length; ++i) {
-            services_list.add(new Service(title[i], price[i]));
-        }
-
-
-        ArrayAdapter<Service> adapter = new ArrayAdapter(this,
-                R.layout.activity_services,
-                android.R.id.list,
-                services_list
-        );
-        ListView listview = findViewById(android.R.id.list);
-        listview.setAdapter(adapter);*//*
-
-    }*/
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -78,6 +32,5 @@ public class ServicesActivity extends AppCompatActivity {
 
         ServicesAdapter itemsAdapter = new ServicesAdapter(this, services_array);
         services_list.setAdapter(itemsAdapter);
-
     }
 }
