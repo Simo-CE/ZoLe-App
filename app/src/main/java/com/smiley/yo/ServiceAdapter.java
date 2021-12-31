9dddfc61c87ea34213f7d19aa835565493eb89b1
chaimaa Taoutaou <taoutaou.chaimaa@gmail.com>
2021-12-31 14:55:25 +0100

    service

--- app/src/main/java/com/smiley/yo/ServiceAdapter.java
+++ app/src/main/java/com/smiley/yo/ServiceAdapter.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -34,6 +36,9 @@ public class ServiceAdapter extends RecyclerView.Adapter<ServiceAdapter.ViewHold
         holder.price.setText(service.getPrice());
         holder.title.setText(service.getTitle());
         holder.desc.setText(service.getDesc());
+
+        Boolean isExpandable=List2.get(position).isExpandables();
+        holder.expandableLayout.setVisibility(isExpandable ? View.VISIBLE : View.GONE);
     }
 
     @Override
@@ -46,12 +51,25 @@ public class ServiceAdapter extends RecyclerView.Adapter<ServiceAdapter.ViewHold
         TextView  title;
         TextView  price;
         TextView  desc;
+        RelativeLayout linear_Layout;
+        RelativeLayout expandableLayout;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
             title=itemView.findViewById(R.id.title);
             price=itemView.findViewById(R.id.price);
             desc=itemView.findViewById(R.id.desc);
+            linear_Layout=itemView.findViewById(R.id.linearLayout);
+            expandableLayout=itemView.findViewById(R.id.expandable_layout);
+
+            linear_Layout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Service service = List2.get(getAdapterPosition());
+                    service.setExpandables(!service.isExpandables());
+                    notifyItemChanged(getAdapterPosition());
+                }
+            });
         }
     }
 }
