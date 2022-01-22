105c98c20331795d5fb8d69a86d94393fcd57145
Simo-CE <70328701+Simo-CE@users.noreply.github.com>
2022-01-22 23:06:52 +0100

    updates

--- app/src/main/java/com/smiley/yo/ProfileFragment.java
+++ app/src/main/java/com/smiley/yo/ProfileFragment.java
@@ -15,11 +15,6 @@ import android.widget.ImageButton;
 
 import com.google.android.material.tabs.TabLayout;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ProfileFragment extends Fragment {
 
     ViewPager viewPager;
@@ -37,15 +32,6 @@ public class ProfileFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ProfileFragment newInstance(String param1, String param2) {
         ProfileFragment fragment = new ProfileFragment();
         Bundle args = new Bundle();
