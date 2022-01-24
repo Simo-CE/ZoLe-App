98415b1a534d013b9f03033a5713ec1810c6713e
Simo-CE <70328701+Simo-CE@users.noreply.github.com>
2022-01-24 21:27:27 +0100

    send mail

--- app/src/main/java/com/smiley/yo/ProfileFragment.java
+++ app/src/main/java/com/smiley/yo/ProfileFragment.java
@@ -19,12 +19,9 @@ public class ProfileFragment extends Fragment {
 
     ViewPager viewPager;
     TabLayout tabLayout;
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
